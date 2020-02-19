7cac5b1e8cada42f0ec2a933b9b7c1542a3cf4ee
afloesch <andrew@vapi.com>
2020-02-18 17:52:53 -0800

    chore: update meeseeks-js and config object

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export default {
+  client_id: "5sXDKBw",
+  client_secret: "ODk4YzZmNDAtMmVmMS00NTA2LTgyZGUtOGZlMDhjMmY0YzczZThlYTgxNzEtMDYxYS00ZjVkLTk5YTctYjU3OGU0ZmQ0NjNi",
+  login_page: "http://localhost:3000/",
+  auth_host: "http://localhost:8000/"
+}
